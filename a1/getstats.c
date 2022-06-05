e03aae97d3f407fb812043334822a62593be9476
Owen Crewe <ocrewe@uvic.ca>
2022-06-05 11:09:05 +0000

    getstats p1 done

--- a1/getstats.c
+++ a1/getstats.c
@@ -9,8 +9,8 @@
  *
  * Please change the following before submission:
  *
- * Author: Luke Skywalker
- * Login:  usetheforce@uvic.ca 
+ * Author: Owen Crewe
+ * Login:  ocrewe@uvic.ca 
  */
 
 
@@ -24,13 +24,80 @@
  * to increase the clarity of your solution, or both.
  */
 
+void print_lines(FILE *fp, int *lines, int lines_len) {
+    if (!lines_len)
+        return;
+    char buffer[256];
+    int line = 0;
+    int printed = 0;
+    while (fgets(buffer, 256, fp) != NULL) {
+        for (int i = 0; i<lines_len; ++i) {
+            if (lines[i] == line) {
+                printf("%s", buffer);
+                printed++;
+            }
+        }
+        line++;
+    }
+}
+
 void print_process_info(char * process_num) {
-    printf("Nothing to see here...\n");
+    
 } 
 
 
 void print_full_info() {
-    printf("Nothing to see here...\n");
+    FILE *cpuinfo;
+    FILE *meminfo;
+    FILE *version;
+    FILE *uptime;
+    int lines[2];
+    
+    cpuinfo = fopen("/proc/cpuinfo", "r"); // model name/cpu cores
+    version = fopen("/proc/version", "r"); // linux version
+    meminfo = fopen("/proc/meminfo", "r"); // MemTotal
+    uptime = fopen("/proc/uptime", "r"); // uptime
+    
+    // Log model name and cpu cores
+    lines[0] = 4;
+    lines[1] = 12;
+    print_lines(cpuinfo, lines, 2);
+    
+    // Log linux version
+    lines[0] = 0;
+    print_lines(version, lines, 1);
+    
+    // Log MemTotal
+    lines[0] = 0;
+    print_lines(meminfo, lines, 1);
+    
+    // Log uptime
+    char c = '0';
+    char buffer[12];
+    int buffer_len = 0;
+    while ((c = fgetc(uptime)) != EOF) {
+        if (c != '.') {
+            buffer[buffer_len++] = c;
+        } else {
+            buffer[buffer_len++] = '\n';
+            buffer[buffer_len++] = '\0';
+            break;
+        }
+    }
+    int seconds = atoi(buffer);
+    int days = seconds/86400;
+    seconds %= 86400;
+    int hours = seconds/3600;
+    seconds %= 3600;
+    int minutes = seconds/60;
+    seconds %= 60;
+    printf("Updtime: %d days, %d hours, %d minutes, %d seconds\n", days, hours, minutes, seconds);
+    
+    // Close files
+    fclose(cpuinfo);
+    fclose(version);
+    fclose(meminfo);
+    fclose(uptime);
 }
 
 
